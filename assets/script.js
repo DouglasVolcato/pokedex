ec96406248bca4e986e12e9423f3b299cac941a4
Douglas Volcato <douglasvolcato@gmail.com>
2022-07-17 02:45:27 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -12,19 +12,14 @@ async function getPokemons(page = 0,number = 99){
         const server1 = await fetch(item.url);
         const data1 = await server1.json();
 
-        //requisitar caract nos links
-        // console.log(data1.stats[0].stat); 
-        // console.log(data1.stats[1].stat);
-        // console.log(data1.stats[2].stat);
-        // console.log(data1.stats[3].stat);
-        // console.log(data1.stats[4].stat);
-        
+        const hp = `${data1.stats[0].stat.name}: ${data1.stats[0].base_stat}`;
+        const attack = `${data1.stats[1].stat.name}: ${data1.stats[1].base_stat}`;
+        const defence = `${data1.stats[2].stat.name}: ${data1.stats[2].base_stat}`;
+        const special_attack = `${data1.stats[3].stat.name}: ${data1.stats[3].base_stat}`;
+        const special_defense = `${data1.stats[4].stat.name}: ${data1.stats[4].base_stat}`;
 
         const name = data1.name;
         const id = data1.id;
-
-
-////EVOLUCAO
 
         let evolutionName1 = "";
         let evolutionName2 = "";
@@ -32,13 +27,20 @@ async function getPokemons(page = 0,number = 99){
         let evolutionId1 = "";
         let evolutionId2 = "";
         let evolutionId3 = "";
+        let evolutionId1Real = "";
+        let evolutionId2Real = "";
+        let evolutionId3Real = "";
+        let evolutionImage1 = "";
+        let evolutionImage2 = "";
+        let evolutionImage3 = "";
+        let evolutionImage1Real = "";
+        let evolutionImage2Real = "";
+        let evolutionImage3Real = "";
 
         try {
-
             const species1 =  await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
             const species2 = await species1.json();
             const species = species2.evolution_chain.url;
-    
             const evolution1 = await fetch(species);
             const evolution = await evolution1.json();
 
@@ -57,7 +59,6 @@ async function getPokemons(page = 0,number = 99){
             } catch(e){
                 evolutionName3 = "Not found";
             }
-
             try{
                 evolutionId1 = evolution.chain.species.url.replace("https://pokeapi.co/api/v2/pokemon-species/", "").replace("/", "").split(' ').join('');
             } catch(e){
@@ -73,21 +74,21 @@ async function getPokemons(page = 0,number = 99){
             } catch(e){
                 evolutionId3 = "Not found";
             }
-            console.log(evolutionName1);
-            console.log(evolutionId1);
-            console.log(evolutionName2);
-            console.log(evolutionId2);
-            console.log(evolutionName3);
-            console.log(evolutionId3);
-            console.log("");
-    
         } 
-        
         catch(e){
             console.log("ERRO 404");
         }
-////
 
+        evolutionId1Real = evolutionId1 == "" ? "Not found" : evolutionId1;
+        evolutionId2Real = evolutionId2 == "" ? "Not found" : evolutionId2;
+        evolutionId3Real = evolutionId3 == "" ? "Not found" : evolutionId3;
+        
+        evolutionImage1 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolutionId1}.png`;
+        evolutionImage2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolutionId2}.png`;
+        evolutionImage3 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolutionId3}.png`;
+        evolutionImage1Real = evolutionImage1 == evolutionId1 == "" ? "./assets/images/noImage.jpg" : evolutionImage1;
+        evolutionImage2Real = evolutionImage2 == evolutionId2 == "" ? "./assets/images/noImage.jpg" : evolutionImage2;
+        evolutionImage3Real = evolutionImage3 == evolutionId3 == "" ? "./assets/images/noImage.jpg" : evolutionImage3;
 
         let image = "";
 
@@ -121,7 +122,83 @@ async function getPokemons(page = 0,number = 99){
             numTypes.push(n);
         };
 
-        if (numTypes.length == 1){
+        let type1 = "";
+        let type2 = "";
+        let type3 = "";
+        let type4 = "";
+        let type5 = "";
+
+        try{
+            type1 = data1.types[0].type.name.toUpperCase();
+        }catch(e){
+            type1 = "";
+        }
+        try{
+            type2 = data1.types[1].type.name.toUpperCase();
+        }catch(e){
+            type2 = "";
+        }
+        try{
+            type3 = data1.types[2].type.name.toUpperCase();
+        }catch(e){
+            type3 = "";
+        }
+        try{
+            type4 = data1.types[3].type.name.toUpperCase();
+        }catch(e){
+            type4 = "";
+        }
+        try{
+            type5 = data1.types[4].type.name.toUpperCase();
+        }catch(e){
+            type5 = "";
+        }
+
+        function verifyType(a){
+            let answer = "";
+            if (a.toUpperCase() == "BUG"){
+                answer = `<a class="type" style="background-color:green; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "DRAGON"){
+                answer = `<a class="type" style="background-color:crimson; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "FAIRY"){
+                answer = `<a class="type" style="background-color:fuchsia; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "FIRE"){
+                answer = `<a class="type" style="background-color:orange">${a}</a>`;
+            } if (a.toUpperCase() == "GHOST"){
+                answer = `<a class="type" style="background-color:darkmagenta; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "GROUND"){
+                answer = `<a class="type" style="background-color:olive; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "NORMAL"){
+                answer = `<a class="type" style="background-color:white">${a}</a>`;
+            } if (a.toUpperCase() == "PSYCHIC"){
+                answer = `<a class="type" style="background-color:pink">${a}</a>`;
+            } if (a.toUpperCase() == "STEEL"){
+                answer = `<a class="type" style="background-color:dimgray; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "DARK"){
+                answer = `<a class="type" style="background-color:black; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "ELECTRIC"){
+                answer = `<a class="type" style="background-color:yellow">${a}</a>`;
+            } if (a.toUpperCase() == "FIGHTING"){
+                answer = `<a class="type" style="background-color:orangered; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "FLYING"){
+                answer = `<a class="type" style="background-color:mediumspringgreen">${a}</a>`;
+            } if (a.toUpperCase() == "GRASS"){
+                answer = `<a class="type" style="background-color:lime">${a}</a>`;
+            } if (a.toUpperCase() == "ICE"){
+                answer = `<a class="type" style="background-color:turquoise">${a}</a>`;
+            } if (a.toUpperCase() == "POISON"){
+                answer = `<a class="type" style="background-color:palevioletred">${a}</a>`;
+            } if (a.toUpperCase() == "ROCK"){
+                answer = `<a class="type" style="background-color:brown; color:white">${a}</a>`;
+            } if (a.toUpperCase() == "WATER"){
+                answer = `<a class="type" style="background-color:steelblue">${a}</a>`;
+            }
+
+            if (answer == undefined){return ""}
+            else {return answer};
+        }
+
+        if (true){
             document.querySelector("#cardList").insertAdjacentHTML("beforeend", `
             
             <div class="card">
@@ -129,7 +206,13 @@ async function getPokemons(page = 0,number = 99){
                 <div class="characterInfo">                    
                     <p class="name">${name.toUpperCase()}</p>
                     <p class="ID">ID: ${id}</p>
-                    <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
+                    <div class="types">
+                        ${verifyType(type1)}
+                        ${verifyType(type2)}
+                        ${verifyType(type3)}
+                        ${verifyType(type4)}
+                        ${verifyType(type5)}
+                    </div>
                     <button type="button" class="showDescription" onclick="showDescription()">Show Description</button>
 
                     <span class="descriptionPokemon">
@@ -137,146 +220,60 @@ async function getPokemons(page = 0,number = 99){
                             <div class="descriptionPokemon1">
 
                                 <button type="button" class="closeDescription" onclick="closeDescription()">Close</button>
-                                <img class="characterImage" src=${image}>
-                                <p>${evolutionName1} ${evolutionId1}, ${evolutionName2} ${evolutionId2}, ${evolutionName3} ${evolutionId3}</p>
-                                <p class="name">${name.toUpperCase()}</p>
-                                <p class="ID">ID: ${id}</p>
-                                <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
-                                <p class="description">${mystring}</p>
 
+                                <div class="divImages">
+                                    <div>
+                                        <img class="characterImage" src=${image}>
+                                        <p class="name">${name.toUpperCase()}</p>
+                                        <p class="ID">ID: ${id}</p>
+                                    </div>
+                                    <div>
+                                        <p class="name">Evolution Chain</p>
+                                        <div class="evolucoes">
+                                            <div>
+                                                <img class="characterImage" src=${evolutionImage1Real}>
+                                                <p class="name">${evolutionName1.toUpperCase()}</p>
+                                                <p class="ID">ID: ${evolutionId1Real}</p>
+                                            </div>
+                                            <div>
+                                            <img class="characterImage" src=${evolutionImage2Real}>
+                                            <p class="name">${evolutionName2.toUpperCase()}</p>
+                                                <p class="ID">ID: ${evolutionId2Real}</p>
+                                            </div>
+                                            <div>
+                                            <img class="characterImage" src=${evolutionImage3Real}>
+                                            <p class="name">${evolutionName3.toUpperCase()}</p>
+                                                <p class="ID">ID: ${evolutionId3Real}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="description_class_stats">
+                                    <div class="stats">
+                                        <p>${hp}</p>
+                                        <p>${attack}</p>
+                                        <p>${defence}</p>
+                                        <p>${special_attack}</p>
+                                        <p>${special_defense}</p>
+                                    </div>
+                                    <div>
+                                        <div class="types">
+                                            ${verifyType(type1)}
+                                            ${verifyType(type2)}
+                                            ${verifyType(type3)}
+                                            ${verifyType(type4)}
+                                            ${verifyType(type5)}
+                                        </div>
+                                        <p class="descriptionPage">${mystring}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </span>
-
                 </div>
             </div>
-
-            `)
-        } else if (numTypes.length == 2){
-            document.querySelector("#cardList").insertAdjacentHTML("beforeend", `
-            
-            <div class="card">
-                <img class="characterImage" src=${image}>
-                <div class="characterInfo">
-                    <p class="name">${name.toUpperCase()}</p>
-                    <p class="ID">ID: ${id}</p>
-                    <p class="type">${data1.types[0].type.name.toUpperCase()}, ${data1.types[1].type.name.toUpperCase()}</p>
-                    <button type="button" class="showDescription" onclick="showDescription()">Show Description</button>
-                    
-                    <span class="descriptionPokemon">
-                        <div class="descriptionPokemon2">
-                            <div class="descriptionPokemon1">
-
-                                <button type="button" class="closeDescription" onclick="closeDescription()">Close</button>
-                                <img class="characterImage" src=${image}>
-                                <p>${evolutionName1} ${evolutionId1}, ${evolutionName2} ${evolutionId2}, ${evolutionName3} ${evolutionId3}</p>
-                                <p class="name">${name.toUpperCase()}</p>
-                                <p class="ID">ID: ${id}</p>
-                                <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
-                                <p class="description">${mystring}</p>
-
-                            </div>
-                        </div>
-                    </span>
-
-                </div>
-            </div>
-
-            `)
-        } else if (numTypes.length == 3){
-            document.querySelector("#cardList").insertAdjacentHTML("beforeend", `
-            
-            <div class="card">
-                <img class="characterImage" src=${image}>
-                <div class="characterInfo">
-                    <p class="name">${name.toUpperCase()}</p>
-                    <p class="ID">ID: ${id}</p>
-                    <p class="type">${data1.types[0].type.name.toUpperCase()}, ${data1.types[1].type.name.toUpperCase()}, ${data1.types[2].type.name.toUpperCase()}</p>
-                    <button type="button" class="showDescription" onclick="showDescription()">Show Description</button>
-                    
-                    <span class="descriptionPokemon">
-                        <div class="descriptionPokemon2">
-                            <div class="descriptionPokemon1">
-
-                                <button type="button" class="closeDescription" onclick="closeDescription()">Close</button>
-                                <img class="characterImage" src=${image}>
-                                <p>${evolutionName1} ${evolutionId1}, ${evolutionName2} ${evolutionId2}, ${evolutionName3} ${evolutionId3}</p>
-                                <p class="name">${name.toUpperCase()}</p>
-                                <p class="ID">ID: ${id}</p>
-                                <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
-                                <p class="description">${mystring}</p>
-
-                            </div>
-                        </div>
-                    </span>
-                    
-                </div>
-            </div>
-
-            `)
-        } else if (numTypes.length == 4){
-            document.querySelector("#cardList").insertAdjacentHTML("beforeend", `
-            
-            <div class="card">
-                <img class="characterImage" src=${image}>
-                <div class="characterInfo">
-                    <p class="name">${name.toUpperCase()}</p>
-                    <p class="ID">ID: ${id}</p>
-                    <p class="type">${data1.types[0].type.name.toUpperCase()}, ${data1.types[1].type.name.toUpperCase()}, ${data1.types[2].type.name.toUpperCase()}, ${data1.types[3].type.name.toUpperCase()}</p>
-                    <button type="button" class="showDescription" onclick="showDescription()">Show Description</button>
-                    
-                    <span class="descriptionPokemon">
-                        <div class="descriptionPokemon2">
-                            <div class="descriptionPokemon1">
-
-                                <button type="button" class="closeDescription" onclick="closeDescription()">Close</button>
-                                <img class="characterImage" src=${image}>
-                                <p>${evolutionName1} ${evolutionId1}, ${evolutionName2} ${evolutionId2}, ${evolutionName3} ${evolutionId3}</p>
-                                <p class="name">${name.toUpperCase()}</p>
-                                <p class="ID">ID: ${id}</p>
-                                <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
-                                <p class="description">${mystring}</p>
-
-                            </div>
-                        </div>
-                    </span>
-                    
-                </div>
-            </div>
-
-            `)
-        } else if (numTypes.length == 5){
-            document.querySelector("#cardList").insertAdjacentHTML("beforeend", `
-            
-            <div class="card">
-                <img class="characterImage" src=${image}>
-                <div class="characterInfo">
-                    <p class="name">${name.toUpperCase()}</p>
-                    <p class="ID">ID: ${id}</p>
-                    <p class="type">${data1.types[0].type.name.toUpperCase()}, ${data1.types[1].type.name.toUpperCase()}, ${data1.types[2].type.name.toUpperCase()}, ${data1.types[3].type.name.toUpperCase()}, ${data1.types[4].type.name.toUpperCase()}</p>
-                    <button type="button" class="showDescription" onclick="showDescription()">Show Description</button>
-                    
-                    <span class="descriptionPokemon">
-                        <div class="descriptionPokemon2">
-                            <div class="descriptionPokemon1">
-
-                                <button type="button" class="closeDescription" onclick="closeDescription()">Close</button>
-                                <img class="characterImage" src=${image}>
-                                <p>${evolutionName1} ${evolutionId1}, ${evolutionName2} ${evolutionId2}, ${evolutionName3} ${evolutionId3}</p>
-                                <p class="name">${name.toUpperCase()}</p>
-                                <p class="ID">ID: ${id}</p>
-                                <p class="type">${data1.types[0].type.name.toUpperCase()}</p>
-                                <p class="description">${mystring}</p>
-
-                            </div>
-                        </div>
-                    </span>
-                    
-                </div>
-            </div>
-
-            `)
-        }
+            `);
+        } 
     });
 }
 
@@ -284,8 +281,6 @@ function showDescription(){
     const buttons = document.querySelectorAll(".showDescription")
     for (let n of buttons){
         n.addEventListener("click", function(){
-            console.log(n.parentElement.querySelector(".ID").innerText.replace("ID:", "").split(' ').join(''));
-
             n.parentElement.querySelector(".descriptionPokemon").style.display="unset";
         });
     }
